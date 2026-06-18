e86b873d836f9589c1fccb5d4b9c7c4bbda5256e
Paweł Mieteń <pawel.mieten@gmail.com>
2026-06-19 00:15:28 +0200

    feat: update styles and configurations for diagnostics and ui-shell modules

--- modules/diagnostics/src/index.ts
+++ modules/diagnostics/src/index.ts
@@ -5,8 +5,8 @@
  * for the eAppFlow UI Shell.
  */
 
-import type { EafModule } from "@eappflow/ui-shell";
-import type { EafMenuModule } from "@eappflow/ui-shell";
+
+import { EafModule } from "@eappflow/ui-shell";
 import type { DiagnosticsConfig } from "./types";
 import { DIAGNOSTICS_CONFIG_KEY } from "./types";
 
